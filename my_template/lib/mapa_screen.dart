35f824a5e1756e4c47bafd6e7404058c48767b2d
terapeutaLPG <terapeutaLPG@gmail.com>
2026-04-16 23:45:54 +0200

    dodanie ekranu mapy i konfiguracja osm

---
+++ my_template/lib/mapa_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:http/http.dart' as http;
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mapa Aptek'),
+        backgroundColor: const Color(0xff2f6df6),
+      ),
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(51.11, 17.03),
+          initialZoom: 13,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _fetchPharmacies() async {}
+}
